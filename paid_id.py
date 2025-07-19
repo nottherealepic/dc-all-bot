146bcb300dcc303496828daae736ed1df5f4cae9
nottherealepic <j4338630@gmail.com>
2025-07-19 16:38:27 +0530

    Create paid_id.py

---
+++ paid_id.py
@@ -0,0 +1,54 @@
+
+paid_id_data = {
+    "epic0000": {
+        "Discord_id": "42424242424",
+        "File_Name": "EPIC_MC_PRO",
+        "For_": "GTA 5",
+        "Date": "20-07-2024",
+        "Via": "UPI",
+        "Othr": "2546"
+    },
+    "epic0001": {
+        "Discord_id": "<@214719348458258433>",
+        "File_Name": "EPIC_MC_PRO",
+        "For_": "GTA 5",
+        "Date": "16-06-2025",
+        "Via": "upi",
+        "Othr": "7562",
+        "Email": "vkhedkar08@gmail.com"
+    },
+    "epic0002": {
+        "Discord_id": "<@1384974271134040134>",
+        "File_Name": "EPIC_MC_PRO",
+        "For_": "GTA 5",
+        "Date": "10-07-2025",
+        "Via": "GIVEAWAY",
+        "Othr": "4827",
+        "Email": "slaktonbucket@gmail.com"
+    },
+    "epic0003": {
+        "Discord_id": "42424242424",
+        "File_Name": "EPIC_MC_PRO",
+        "For_": "GTA 5",
+        "Date": "20-07-2024",
+        "Via": "WISE",
+        "Othr": "9587"
+    },
+    "epic0004": {
+        "Discord_id": "42424242424",
+        "File_Name": "EPIC_MC_PRO",
+        "For_": "GTA 5",
+        "Date": "20-07-2024",
+        "Via": "WISE",
+        "Othr": "2452"
+    },
+    "epic0005": {
+        "Discord_id": "42424242424",
+        "File_Name": "EPIC_MC_PRO",
+        "For_": "GTA 5",
+        "Date": "20-07-2024",
+        "Via": "WISE",
+        "Othr": "5262"
+    },
+    
+}
