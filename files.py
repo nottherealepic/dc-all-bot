01d9e9eee70b799581251b88c06430937bd558af
nottherealepic <j4338630@gmail.com>
2025-07-19 16:36:03 +0530

    Create files.py

---
+++ files.py
@@ -0,0 +1,21 @@
+# files.py
+
+files_data = {
+    "ZedOne_Beta_Free": {
+        "size": "17MB",
+        "version": "beta",
+        "for": "GTA 5",
+        "last_update": "20-07-2024",
+        "license": "Personal Use License",
+        "password": "7612"
+    },
+    "ZedOne_Beta_Free_3D_Model": {
+        "size": "55MB",
+        "version": "beta",
+        "for": "3D model",
+        "last_update": "27-07-2024",
+        "license": "Creative License",
+        "password": "7685"
+    },
+    
+}
