fb9b8f043f51855a3ff767e23e491e8f75423d9a
nottherealepic <j4338630@gmail.com>
2025-07-19 16:30:01 +0530

    Create licence.py

---
+++ licence.py
@@ -0,0 +1,7 @@
+# licence.py
+
+license_descriptions = {
+    "Personal Use License" : "Products with a personal use license are strictly for individual use Editing or redistribution of these products is prohibited without prior written consent.",
+    "Creative License" : "Certain paid products may come with a creative license, allowing modification and redistribution for non-commercial purposes only, provided proper credit is given to the original creator.This license applies solely to specific products as mentioned in the license details.",
+    "Commercial Use License" : "Some paid products are licensed for modification and redistribution for commercial purposes, provided proper credit is attributed to the original creator.Only the products explicitly mentioned as eligible for commercial licensing fall under this category.",
+}
